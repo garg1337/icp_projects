e9d10d5f19b671629bb03c2e4ac288192742bf44
arjun garg <arjunpgarg@gmail.com>
2022-01-16 13:38:17 -0800

    added rust profile

---
+++ rust_profile/src/declarations/rust_profile/rust_profile.did.d.ts
@@ -0,0 +1,13 @@
+import type { Principal } from '@dfinity/principal';
+export type Profile = Profile_2;
+export interface Profile_2 {
+  'name' : string,
+  'description' : string,
+  'keywords' : Array<string>,
+}
+export interface _SERVICE {
+  'get' : (arg_0: string) => Promise<Profile_2>,
+  'getSelf' : () => Promise<Profile_2>,
+  'search' : (arg_0: string) => Promise<[] | [Profile_2]>,
+  'update' : (arg_0: Profile_2) => Promise<undefined>,
+}
---
+++ rust_profile/src/declarations/rust_profile/rust_profile.did.js
@@ -0,0 +1,14 @@
+export const idlFactory = ({ IDL }) => {
+  const Profile_2 = IDL.Record({
+    'name' : IDL.Text,
+    'description' : IDL.Text,
+    'keywords' : IDL.Vec(IDL.Text),
+  });
+  return IDL.Service({
+    'get' : IDL.Func([IDL.Text], [Profile_2], ['query']),
+    'getSelf' : IDL.Func([], [Profile_2], ['query']),
+    'search' : IDL.Func([IDL.Text], [IDL.Opt(Profile_2)], ['query']),
+    'update' : IDL.Func([Profile_2], [], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
